fe700eefecb682c5e4872a6a5a7afdd3d0abb81e
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-01-10 19:45:12 -0500

    isolating relevant info for individual subreddits

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,15 +1,25 @@
 import React from 'react';
 
 class Test extends React.Component {
-  state = {};
+  state = {
+    subreddits: []
+  };
 
   componentDidMount() {
-    return fetch(`https://www.reddit.com/search.json?q=architecture&restrict_sr=&sort=relevance&t=all&type=sr&count=0&after=t5_3993a`)
+    return fetch(`https://www.reddit.com/subreddits/search.json?q=architecture`)
       .then(res => res.json())
-      .then(res => console.log(res.data.children));
+      .then(res =>
+        this.setState({
+          subreddits: res.data.children
+        })
+      );
   }
 
   render() {
+    console.log(this.state.subreddits);
+    this.state.subreddits.map(subreddit => {
+      console.log(`${subreddit.data.display_name_prefixed}, ${subreddit.data.public_description} subscribers: ${subreddit.data.subscribers}`);
+    });
     return (
       <div>
         <h1>Test</h1>
