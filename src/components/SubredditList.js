68ecf7a30d401a510e791dd0384673fff025a404
Jesse Tyner-Bryan <jesse.tynerbryan@gmail.com>
2018-01-13 13:36:41 -0500

    autocomplete implemented

--- src/components/SubredditList.js
+++ src/components/SubredditList.js
@@ -16,24 +16,32 @@ class SubredditList extends React.Component {
   };
 
   render() {
-    let subredditList = this.props.subredditList.subreddits.slice(1).map((subreddit, index) => {
+    if (Object.keys(this.props.subredditList).length === 2) {
+      let subredditList = this.props.subredditList.subreddits.slice(1).map((subreddit, index) => {
+        return (
+          <div key={index} className="subreddit" onClick={() => this.handleSelectedSubReddit(subreddit.data.display_name_prefixed)}>
+            <h1>{subreddit.data.display_name_prefixed}</h1>
+            <h2>{subreddit.data.public_description}</h2>
+            <h3>{subreddit.data.subscribers} subscribers</h3>
+          </div>
+        );
+      });
+
       return (
-        <div key={index} className="subreddit" onClick={() => this.handleSelectedSubReddit(subreddit.data.display_name_prefixed)}>
-          <h1>{subreddit.data.display_name_prefixed}</h1>
-          <h2>{subreddit.data.public_description}</h2>
-          <h3>{subreddit.data.subscribers} subscribers</h3>
+        <div>
+          <button onClick={this.handleClick}>all topics</button>
+          <h1>{this.props.subredditList.topic.toUpperCase()}</h1>
+          <h3>click on subreddits for preview</h3>
+          {subredditList}
         </div>
       );
-    });
-
-    return (
-      <div>
-        <button onClick={this.handleClick}>all topics</button>
-        <h1>{this.props.subredditList.topic.toUpperCase()}</h1>
-        <h3>click on subreddits for preview</h3>
-        {subredditList}
-      </div>
-    );
+    } else {
+      return (
+        <div>
+          <h1>Fetching Related Subreddits...</h1>
+        </div>
+      );
+    }
   }
 }
 
--- src/components/TopicsList.js
+++ src/components/TopicsList.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { setSubredditListToRender } from '../actions/index';
+import ReactAutocomplete from 'react-autocomplete';
 
 class TopicsList extends React.Component {
+  state = {
+    value: '',
+    topics: []
+  };
+
   componentDidMount() {
     if (this.props.topics.topicsWithSubreddits.length === 0) {
       this.props.history.push('/');
+    } else {
+      let topics = this.props.topics.topicsWithSubreddits.map(topic => {
+        return { label: topic.name };
+      });
+      this.setState({
+        topics: topics
+      });
     }
   }
 
@@ -14,6 +27,19 @@ class TopicsList extends React.Component {
     this.props.setSubredditListToRender(topic, this.props.topics.topicsWithSubreddits[index]);
 
     this.props.history.push('/subreddit-list');
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    let value = this.state.value;
+    let topics = this.props.topics.topicsWithSubreddits.map(topic => {
+      return topic.name;
+    });
+    if (topics.includes(value)) {
+      this.props.setSubredditListToRender(value, this.props.topics.topicsWithSubreddits[topics.indexOf(value)]);
+
+      this.props.history.push('/subreddit-list');
+    }
   };
 
   render() {
@@ -29,6 +55,23 @@ class TopicsList extends React.Component {
     return (
       <div>
         <h1>Topic List (click for relevant subreddits)</h1>
+        <h4>Search Topics</h4>
+        <form onSubmit={this.handleSubmit}>
+          <ReactAutocomplete
+            items={this.state.topics}
+            shouldItemRender={(item, value) => item.label.toLowerCase().indexOf(value.toLowerCase()) > -1}
+            getItemValue={item => item.label}
+            renderItem={(item, highlighted) => (
+              <div key={item.id} style={{ backgroundColor: highlighted ? 'white' : 'transparent', color: 'black' }}>
+                {item.label}
+              </div>
+            )}
+            value={this.state.value}
+            onChange={e => this.setState({ value: e.target.value })}
+            onSelect={value => this.setState({ value })}
+          />
+          <input type="submit" value="submit" />
+        </form>
         {topicList}
       </div>
     );
